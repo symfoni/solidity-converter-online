684369a2a64c40e9eeb9a8fc8abe0a8e1ec2139e
Robertosnap <robertosnap@pm.me>
2019-05-21 12:33:35 +0200

    github pages

---
+++ precache-manifest.9c992e4175252c3825f691d83af4ceab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/solidity-converter-online/robots.txt"
+  },
+  {
+    "revision": "b338d2551509125a17c5",
+    "url": "/solidity-converter-online/js/chunk-vendors.d149e4a3.js"
+  },
+  {
+    "revision": "dd6c7dd346cb96061906",
+    "url": "/solidity-converter-online/js/app.83d23ddf.js"
+  },
+  {
+    "revision": "5dbab570b5cbced59db7286eb40c2c60",
+    "url": "/solidity-converter-online/index.html"
+  },
+  {
+    "revision": "c2e071983d6f345d5a819b99a421727a",
+    "url": "/solidity-converter-online/img/blockchangers-block.c2e07198.png"
+  },
+  {
+    "revision": "b338d2551509125a17c5",
+    "url": "/solidity-converter-online/css/chunk-vendors.19588e8d.css"
+  },
+  {
+    "revision": "dd6c7dd346cb96061906",
+    "url": "/solidity-converter-online/css/app.948ee19d.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d20ccafe3b58e7ee18bd5d31e7a7613.js"
+  "/solidity-converter-online/precache-manifest.9c992e4175252c3825f691d83af4ceab.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "solidity-converter"});
