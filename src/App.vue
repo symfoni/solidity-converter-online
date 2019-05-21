1359d8695b64fe6021c9f366da5662850c683c80
Robertosnap <robertosnap@pm.me>
2019-05-21 15:47:46 +0200

    'deploying'

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <!-- <b-nav-item href="#">Link</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>-->
+          <!-- <b-nav-item href="#">Link</b-nav-item> -->
+          <b-nav-item href="https://www.blockchangers.com/" >Blockchangers</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
