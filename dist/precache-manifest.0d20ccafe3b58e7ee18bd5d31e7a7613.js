2aa1041c9204e2365af74f42b3aa50453a4ac070
Robertosnap <robertosnap@pm.me>
2019-05-21 12:25:25 +0200

    sec commit

---
+++ dist/precache-manifest.0d20ccafe3b58e7ee18bd5d31e7a7613.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f045f1ca877c86b6cd17",
+    "url": "/js/chunk-vendors.f2ef521e.js"
+  },
+  {
+    "revision": "76285f5cf5b66a13920d",
+    "url": "/js/app.d046d9f5.js"
+  },
+  {
+    "revision": "768f0916f5c89692194a103351933c88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2e071983d6f345d5a819b99a421727a",
+    "url": "/img/blockchangers-block.c2e07198.png"
+  },
+  {
+    "revision": "f045f1ca877c86b6cd17",
+    "url": "/css/chunk-vendors.19588e8d.css"
+  },
+  {
+    "revision": "76285f5cf5b66a13920d",
+    "url": "/css/app.948ee19d.css"
+  }
+];
